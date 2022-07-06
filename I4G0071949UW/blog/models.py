3598b20d221c56b0e86a5c037d4fbc000b89f0e1
muftiatbakare <bakare.muftiat@gmail.com>
2022-07-06 12:37:33 -0700

    corrections

--- I4G0071949UW/blog/models.py
+++ I4G0071949UW/blog/models.py
@@ -1,9 +1,23 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
+from django.urls import reverse
 from django.conf import settings
 # Create your models here.
-class blog(models.Model):
+
+class Post(models.Model):
     title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, related_name="blog_posts"
+    )
     text = models.TextField()
-    Created_date = models.DateTimeField()
-    Published_date  = models.DateTimeField()
-    author = author = models.ForeignKey(get_user_model())
+
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:post_detail ")
